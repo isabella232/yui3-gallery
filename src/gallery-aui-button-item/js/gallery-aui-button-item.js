ce7233e57f8b57d4a511855691ca22e6a6c9030f
Nate Cavanaugh <nathan.cavanaugh@liferay.com>
2012-03-13 18:19:32 -0700

    AUI-402, AUI-410, AUI-498

--- src/gallery-aui-button-item/js/gallery-aui-button-item.js
+++ src/gallery-aui-button-item/js/gallery-aui-button-item.js
@@ -5,20 +5,27 @@
  */
 
 var Lang = A.Lang,
+	isString = Lang.isString,
 
 	getClassName = A.ClassNameManager.getClassName,
 
 	NAME = 'buttonitem',
 
+	BOUNDING_BOX = 'boundingBox',
+	BUTTON = 'button',
 	CONTENT_BOX = 'contentBox',
 	DOT = '.',
+	HANDLER = 'handler',
 	ICON = 'icon',
 	ICON_NODE = 'iconNode',
 	LABEL = 'label',
 	LABEL_NODE = 'labelNode',
 	ONLY = 'only',
+	RESET = 'reset',
 	STATE = 'state',
+	SUBMIT = 'submit',
 	TITLE = 'title',
+	TYPE = 'type',
 
 	CSS_BUTTON = getClassName(NAME),
 	CSS_BUTTON_ICON = getClassName(NAME, ICON),
@@ -77,6 +84,16 @@ var ButtonItem = A.Component.create(
 		NAME: NAME,
 
 		/**
+		 * Static property provides an array to specify which classes augment this one.
+		 *
+		 * @property ButtonItem.AUGMENTS
+		 * @type array
+		 * @static
+		 */
+
+		AUGMENTS: [A.WidgetChild],
+
+		/**
 		 * Static property used to define the default attribute
 		 * configuration for the ButtonItem.
 		 *
@@ -129,29 +146,7 @@ var ButtonItem = A.Component.create(
 			 */
 			handler: {
 				lazyAdd: false,
-				value: null,
-				setter: function(value) {
-					var instance = this;
-
-					var fn = value;
-					var context = instance;
-					var args = instance;
-					var type = 'click';
-
-					if (Lang.isObject(fn)) {
-						var handlerConfig = fn;
-
-						fn = handlerConfig.fn || fn;
-						context = handlerConfig.context || context;
-						type = handlerConfig.type || type;
-					}
-
-					if (Lang.isFunction(fn)) {
-						instance.on(type, A.rbind(fn, context, args, handlerConfig.args));
-					}
-
-					return value;
-				}
+				value: null
 			},
 
 			/**
@@ -234,6 +229,20 @@ var ButtonItem = A.Component.create(
 			title: {
 				setter: '_setTitle',
 				value: false
+			},
+
+			/**
+			 * Button type.
+			 *
+			 * @attribute type
+			 * @default button
+			 * @type String
+			 */
+			type: {
+				validator: function(val) {
+					return (val === BUTTON || val === SUBMIT || val === RESET);
+				},
+				value: BUTTON
 			}
 		},
 
@@ -276,7 +285,7 @@ var ButtonItem = A.Component.create(
 		},
 
 		constructor: function(config) {
-			if (Lang.isString(config)) {
+			if (isString(config)) {
 				config = {
 					icon: config
 				};
@@ -285,7 +294,7 @@ var ButtonItem = A.Component.create(
 			ButtonItem.superclass.constructor.call(this, config);
 		},
 
-		UI_ATTRS: [ICON, LABEL, TITLE],
+		UI_ATTRS: [HANDLER, ICON, LABEL, TITLE, TYPE],
 
 		prototype: {
 			BOUNDING_TEMPLATE: TPL_BUTTON,
@@ -319,7 +328,7 @@ var ButtonItem = A.Component.create(
 				var title = instance.get('title');
 
 				if (icon) {
-					instance._uiSetIcon(icon)
+					instance._uiSetIcon(icon);
 				}
 
 				if (label) {
@@ -452,11 +461,53 @@ var ButtonItem = A.Component.create(
 				var hasLabelOnly = (!icon && label);
 				var hasIconOnly = (icon && !label);
 
-				var boundingBox = instance.get('boundingBox');
+				var boundingBox = instance.get(BOUNDING_BOX);
 
 				boundingBox.toggleClass(CSS_BUTTON_ICON_LABEL, hasIconAndLabel);
 				boundingBox.toggleClass(CSS_BUTTON_ICON_ONLY, hasIconOnly);
 				boundingBox.toggleClass(CSS_BUTTON_LABEL_ONLY, hasLabelOnly);
+			},
+
+			/**
+			 * Updates the UI for the icon in response to the <a href="ButtonItem.html#event_HandlerChange">Handler</a> event.
+			 *
+			 * @method _uiSetHandler
+			 * @param {String} val Handler name
+			 * @protected
+			 */
+			_uiSetHandler: function(value) {
+				var instance = this;
+
+				var fn = value;
+				var parent = instance.get('parent');
+				var context = (parent && parent._DEFAULT_CONTEXT) || instance._DEFAULT_CONTEXT || instance;
+
+				var type = 'click';
+
+				var args = instance;
+				var customArgs;
+
+				if (Lang.isObject(fn)) {
+					var handlerConfig = fn;
+
+					fn = handlerConfig.fn || fn;
+					context = handlerConfig.context || context;
+					type = handlerConfig.type || type;
+
+					customArgs = handlerConfig.args;
+				}
+
+				if (Lang.isFunction(fn)) {
+					var interactionHandle = instance._interactionHandle;
+
+					if (interactionHandle) {
+						interactionHandle.detach();
+					}
+
+					var boundFn = A.rbind.apply(A, [fn, context, args].concat(customArgs || []));
+
+					instance._interactionHandle = instance.on(type, boundFn);
+				}
 			},
 
 			/**
@@ -523,12 +574,27 @@ var ButtonItem = A.Component.create(
 			_uiSetTitle: function(val) {
 				var instance = this;
 
-				var boundingBox = instance.get('boundingBox');
+				var boundingBox = instance.get(BOUNDING_BOX);
 
-				boundingBox.setAttribute('title', val);
+				boundingBox.setAttribute(TITLE, val);
+			},
+
+			/**
+			 * Updates the UI for the type in response to the <a href="ButtonItem.html#event_typeChange">typeChange</a> event.
+			 *
+			 * @method _uiSetType
+			 * @param {String} val The new value
+			 * @protected
+			 */
+			_uiSetType: function(val) {
+				var instance = this;
+
+				var boundingBox = instance.get(BOUNDING_BOX);
+
+				boundingBox.setAttribute(TYPE, val);
 			}
 		}
 	}
 );
 
-A.ButtonItem = A.Base.build(NAME, ButtonItem, [A.WidgetChild], { dynamic: false });
+A.ButtonItem = ButtonItem;
